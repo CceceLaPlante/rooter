d73f8b1944d613fae4e02c54f873e6580070d217
CeceLaPlante <103273947+CceceLaPlante@users.noreply.github.com>
2022-12-16 23:00:03 +0100

    Update router_simple.adb
    j'avais oublié d'enlever le TODO -_-''

--- router_simple.adb
+++ router_simple.adb
@@ -5,9 +5,6 @@ with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO;  use Ada.Text_IO.Unbounded_IO;
 with Ada.Command_Line;          use Ada.Command_Line;
 with Ada.Exceptions;            use Ada.Exceptions;
----------------------------
--- TODO !!rajouter les fonction To_Unbounded_String et To_String !!
----------------------------
 
 procedure Routeur_Simple is 
 
