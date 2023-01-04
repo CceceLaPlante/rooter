f30318d998bd3ba8aaffe4272a4393b0a01a370b
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2023-01-04 20:17:24 +0100

    Add files via upload

---
+++ Cache_Exceptions.ads
@@ -0,0 +1,6 @@
+-- Définition d'une exception commune pour toutes les caches fait de listes chaînées.
+package Cache_Exceptions is
+
+	Adresse_Absente_Exception  : Exception;	-- une adresse est absente du cache
+
+end Cache_Exceptions;
