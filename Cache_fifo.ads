f0ae0411ce50bf4e728485828df9d662a4a462aa
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2023-01-04 20:17:01 +0100

    Update Cache_fifo.ads

--- Cache_fifo.ads
+++ Cache_fifo.ads
@@ -3,21 +3,30 @@ with LCA ;
 generic
     type T_Stats is private ;
     type T_Adresse is private ;
-    type T_Cellule_Stats is private ;
-
+    type T_Cellule is private ;
+    type T_Interface is private ;
 
 package Cache_fifo is
-    type T_LCA_Stats is limited private ;
-    function Est_Vide(Cache : in T_LCA_Stats) return Boolean ;
-    procedure Initialiser(Cache : out T_LCA_Stats) with
-        Post => Taille(Cache) = 1 and Cache.all.Stats = 0 ;
-    procedure Supprimer(Cache : in out T_LCA_Stats) ;
-    procedure Enregistrer(Cache : in out T_LCA_Stats) with
-        Pre => not Est_Pleine(Cache) ;
+
+    type T_Complet is limited private ;
+
+    procedure Initialiser(Cache : out T_Complet) with
+        Post => Taille(Cache) = 1 and Cache.Stats = 0 ;
+
+    function Est_Vide(Cache : in T_Complet) return Boolean ;
+
+    procedure Supprimer(Cache : in out T_Complet) ;
+
+    procedure Enregistrer(Cache_Contenu : in out T_LCA, Cache_Stats : in out T_Stats, Adresse_IP : in T_Adresse, Interface_Adresse : in T_Interface) ;
+
     procedure Free is new Ada.Unchecked_Deallocation (Object => T_Table, Name => T_Liste);
-    -- function La_Cle(Cache : in T_Complet, Donnee : in T_Adresse) return Integer;
-    procedure Vider(Cache : in out T_LCA_Stats);
-        Post => Est_Vide(Cache) ;
+
+    procedure Vider(Cache_Contenu : in out T_LCA, Cache_Stats : in out T_Stats);
+        -- Post => Est_Vide(Cache) ;
+
+    function Adresse_Presente(Cache_Contenu : in T_LCA, Cache_Stats : in T_Stats, Adresse : in T_Adresse) return Boolean;
+
+    function Taille(Cache_Contenu : in T_LCA, Cache_Stats : in T_Stats) return Integer ;
 
 
 private
@@ -29,16 +38,20 @@ private
     end record ;
 
     type T_Adresse ;
+    type T_Interface ;
 
-    type T_Cellule_Stats ;
+    type T_Cellule ;
+    type T_LCA is access T_Cellule ;
+    type T_Cellule is record
+        Adresse : T_Adresse ;
+        Interface : T_Interface ; 
+        Cle : T_Cle ;
+        Suivant : T_LCA ;
+    end record ;
     
-    type T_LCA_Stats is access T_Cellule_Stats ;
-    
-    type T_Cellule_Stats is record
-        Donnee : T_Adresse ;
+    type T_Complet is record
         Stats : T_Stats ;
-        Cle : Integer ;
-        Suivant : T_LCA_Stats ;
+        Contenu : T_LCA ;
     end record ;
 
 end Cache_fifo ;
