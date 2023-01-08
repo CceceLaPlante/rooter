9b88032bf3313b9e64c8c20c03883630c99fde2d
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2023-01-08 10:49:23 +0100

    Update Cache_fifo.ads

--- Cache_fifo.ads
+++ Cache_fifo.ads
@@ -6,40 +6,45 @@ generic
    capacite_cache : Integer ;
 
 package Cache_fifo is
-   
+
+   type T_Stats is record
+      nb_demandes : Float ;
+      nb_defauts : Float ;
+      taux_defauts : Float ;
+   end record ;
+
+   --type T_Complet is record
+   --Stats : T_Stats ;
+   --Contenu : T_Cellule ;
+   --end record ;
+
    type T_LCA is private ;
-   
-   type T_Stats is private ;
-   type T_Complet is limited private ;
 
-   procedure Initialiser(Cache : out T_Complet) ;
-   -- Post => Taille(Cache) = 1 and Cache.Stats = 0 ;
+   --type T_Stats is private ;
+   --type T_Complet is limited private ;
 
-   function Est_Vide(Cache : in T_Complet) return Boolean ;
+   procedure Initialiser(Cache : out T_LCA; Stats : out T_Stats) ;
+   -- Post => Taille(Cache) = 1 and Stats = 0 ;
 
-   procedure Supprimer(Cache : in out T_Complet) ;
+   function Est_Vide(Cache : in T_LCA) return Boolean ;
 
-   procedure Enregistrer(Cache_Contenu : in out T_LCA; Cache_Stats : in out T_Stats; Adresse_IP : in Unbounded_String; Interface_Adresse : in String) ;
+   procedure Supprimer(Cache : in out T_LCA) ;
+
+   procedure Enregistrer(Cache : in out T_LCA; Stats : in out T_Stats; Adresse_IP : in Unbounded_String; Interface_Adresse : in Unbounded_String) ;
 
    --procedure Free is new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_LCA);
 
-   procedure Vider(Cache_Contenu : in out T_LCA; Cache_Stats : in out T_Stats);
+   procedure Vider(Cache : in out T_LCA; Stats : in out T_Stats);
    -- Post => Est_Vide(Cache) ;
 
-   function Adresse_Presente(Cache_Contenu : in T_LCA; Cache_Stats : in T_Stats; Adresse : in Unbounded_String) return Boolean;
+   function Adresse_Presente(Cache : in T_LCA; Stats : in T_Stats; Adresse : in Unbounded_String) return Boolean;
 
-   function Taille(Cache_Contenu : in T_LCA; Cache_Stats : in T_Stats) return Integer ;
-   
-   function Est_Pleine(Cache : in T_Complet; capacite_cache : in Integer) return Boolean ;
+   function Taille(Cache : in T_LCA) return Integer ;
+
+   function Est_Pleine(Cache : in T_LCA; capacite_cache : in Integer) return Boolean ;
 
 
 private
-
-   type T_Stats is record
-      nb_demandes : Integer ;
-      nb_defauts : Integer ;
-      taux_defauts : Float ;
-   end record ;
 
    type T_Cellule ;
    type T_LCA is access T_Cellule ;
@@ -48,11 +53,6 @@ private
       Interface_utilisation : Unbounded_String ; 
       Cle : Integer ;
       Suivant : T_LCA ;
-   end record ;
-    
-   type T_Complet is record
-      Stats : T_Stats ;
-      Contenu : T_LCA ;
    end record ;
 
 end Cache_fifo ;
