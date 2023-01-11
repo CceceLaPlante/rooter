891f661612110fbf61924ba55a6801ef8efb5ec7
hannabkre <111706622+hannabkre@users.noreply.github.com>
2023-01-11 10:34:39 +0100

    Update test_cache_ll.adb
    test final - fonctionne yay!

--- test_cache_ll.adb
+++ test_cache_ll.adb
@@ -3,24 +3,26 @@ with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
 with SDA_Exceptions;         use SDA_Exceptions;
 with Ada.Calendar; use Ada.Calendar;
 with cache_exception; use cache_exception ;
+with Ada.Text_IO; use Ada.Text_IO;
 
 
 procedure test_cache_ll is 
 
-   package cache_ll_utilisation is new Cache_fifo(capacite_cache => 5) ;
+   package cache_ll_utilisation is new cache_ll(capacite_cache => 5) ;
    use cache_ll_utilisation;
    
-   procedure Afficher_cache is new Cache_fifo_utilisation.Pour_Chaque(Afficher);
    
    procedure Afficher (adresse : in Unbounded_String ; interface_utilisation : in Unbounded_String) is
     begin
         Put("Adresse : ");
-        Put(To_Unbounded_String(adresse));
+        Put(To_String(adresse));
         Skip_Line ;
         Put("interface : ");
-        Put(To_Unbounded_String(interface_utilisation));
+        Put(To_String(interface_utilisation));
         Skip_Line ;
     end Afficher ;
+
+   procedure Afficher_cache is new cache_ll_utilisation.Pour_Chaque(Afficher);
 
    Un_Cache : T_LCA ;
    Stats : T_Stats ;
@@ -28,7 +30,7 @@ procedure test_cache_ll is
    min : Unbounded_String;
    freq_min : Integer ;
    max : Unbounded_String ;
-   temps_max : out Horaire ;
+   temps_max : Horaire ;
    
 begin
 
@@ -38,6 +40,7 @@ begin
    
    Enregistrer(Un_Cache,Stats,To_Unbounded_String("147.255.32.57"),To_Unbounded_String("eth0")) ;
    Afficher_cache(Un_Cache);
+   Put("Etape 1");
    --pragma assert (Adresse_Presente(Un_Cache,Stats, "147.255.32.57")) ;
    --pragma assert (Un_Cache.all.Adresse = "147.255.32.57") ;
    --pragma assert (Un_Cache.all.Interface_utilisation = "eth0") ;
@@ -49,6 +52,7 @@ begin
    
    Enregistrer(Un_Cache,Stats,To_Unbounded_String("147.255.65.36"),To_Unbounded_String("eth1")) ;
    Afficher_cache(Un_Cache);
+   Put("Etape 2");
    --pragma assert (Un_Cache.all.Adresse = "147.255.32.57") ;
    --pragma assert (Un_Cache.all.Interface_utilisation = "eth0") ;
    --pragma assert (Un_Cache.all.Suivant.all.Adresse = "147.255.65.36") ;
@@ -61,6 +65,7 @@ begin
    
    Enregistrer(Un_Cache,Stats,To_Unbounded_String("147.255.32.57"), To_Unbounded_String("eth0")) ;
    Afficher_cache(Un_Cache);
+   Put("Etape 3");
    --pragma assert (Un_Cache.all.Adresse = "147.255.32.57") ;
    --pragma assert (Un_Cache.all.Interface_utilisation = "eth0") ;
    --pragma assert (Un_Cache.all.Suivant.all.Adresse = "147.255.65.36") ;
@@ -75,17 +80,21 @@ begin
    
    Enregistrer(Un_Cache,Stats,To_Unbounded_String("152.215.98.57"),To_Unbounded_String("eth2")) ;
    Afficher_cache(Un_Cache);
+   Put("Etape 4");
    
    Enregistrer(Un_Cache,Stats,To_Unbounded_String("136.145.6.63"),To_Unbounded_String("eth3")) ;
    Afficher_cache(Un_Cache);
-   
+   Put("Etape 5");
+
    Enregistrer(Un_Cache,Stats,To_Unbounded_String("254.168.15.76"),To_Unbounded_String("eth4")) ;
    Afficher_cache(Un_Cache);
+   Put("Etape 6");
    pragma assert (Taille(Un_Cache) = 5) ;
    pragma assert (Est_Pleine(Un_Cache, capacite_cache)) ;
    
    Supprimer_fifo(Un_Cache);
    Afficher_cache(Un_Cache);
+   Put("Etape 7");
    --pragma assert (Un_Cache.all.Adresse = "147.255.65.36") ;
    --pragma assert (Un_Cache.all.Interface_utilisation = "eth1") ;
    pragma assert (Taille(Un_Cache) = 4) ;
@@ -95,18 +104,23 @@ begin
    Chercher_min_freq(Un_Cache, min, freq_min);
    Supprimer_lfu(Un_Cache,min);
    Afficher_cache(Un_Cache);
+   Put("Etape 8");
    pragma assert (Taille(Un_Cache) = 3) ;
    pragma assert (not Est_Vide(Un_Cache)) ;
    pragma assert (not Est_Pleine(Un_Cache,capacite_cache)) ;
    
-   Chercher_min_freq(Un_Cache, max, temps_max);
+   Chercher_max_temps(Un_Cache, max, temps_max);
    Supprimer_lru(Un_Cache,max);
    Afficher_cache(Un_Cache);
+   Put("Etape 9");
    pragma assert (Taille(Un_Cache) = 2) ;
    pragma assert (not Est_Vide(Un_Cache)) ;
    pragma assert (not Est_Pleine(Un_Cache,capacite_cache)) ;
    
    Vider(Un_Cache, Stats) ; 
+   pragma assert (Est_Vide(Un_Cache));
+   pragma assert (Taille(Un_Cache) = 0);
+   pragma assert (not Est_Pleine(Un_Cache, capacite_cache));
    
 end test_cache_ll;
 
