1ca93a572455afd6aaccb2fdd3aecd804d24988c
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2023-01-13 16:54:55 +0100

    Update routeur_la.adb
    rooter en construction, pas prise en compte du masque donc l'ajout

--- routeur_la.adb
+++ routeur_la.adb
@@ -5,7 +5,7 @@ with Ada.Text_IO.Unbounded_IO;  use Ada.Text_IO.Unbounded_IO;
 --with Ada.Command_Line;          use Ada.Command_Line;
 --with Ada.Exceptions;            use Ada.Exceptions;
 with Ada.Unchecked_Deallocation; 
-with cache_ll; use cache_ll;
+with cache_la; use cache_la;
 
 procedure routeur_la is 
 
