5878e2195b33811953c3e39917e626f432ddef18
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2023-01-14 11:37:10 +0100

    Update cache_ll.adb

--- cache_ll.adb
+++ cache_ll.adb
@@ -115,7 +115,35 @@ package body cache_ll is
          Stats.taux_defauts := 0.0 ;
       end if;
    end Vider ;
-
+   
+   function Presence_masque(Cache : in T_LCA; Adresse_IP_entree : in Unbounded_String) return Boolean is
+      Adresse_IP_entree_masque : Unbounded_String;
+      Adresse_IP_courante_masque : Unbounded_String;
+      Masque_IP_courante : Unbounded_String;
+      Adresse_IP_courante : Unbounded_String;
+   begin
+      if Est_Vide(Cache) then
+         return False;
+      else
+         Adresse_IP_courante := Cache.all.Adresse;
+         Masque_IP_courante := Cache.all.Masque;
+         Adresse_IP_entree_masque := Adresse_IP_entree;
+         Adresse_IP_courante_masque := Adresse_IP_courante;
+         for i in 1..length(Adresse_IP_courante) loop
+            if Masque_IP_courante(i) = 0 then
+               Adresse_IP_courante_masque(i) := 0;
+               Adresse_IP_entree_masque(i) := 0;
+            else
+               Null;
+            end if;
+         end loop;
+         if Adresse_IP_courante_masque /= Adresse_IP_entree_masque then
+            return Adresse_Correspondante(Cache.all.Suivant,Adresse_IP_entree);
+         else
+            return True ;
+         end if;
+      end if;
+end Presence_masque;
 
    function Adresse_Presente(Cache : in T_LCA; Stats : in T_Stats; Adresse : in Unbounded_String; Masque_Adresse: in Unbounded_String) return Boolean is
    begin
