8dbdf466599ae1aa474a8ffa8dfb6db056d06198
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2023-01-14 12:03:21 +0100

    Appel cache avant table pour cache et interface

--- routeur_ll.adb
+++ routeur_ll.adb
@@ -460,8 +460,8 @@ begin
             end if;
             
             if not Adresse_Presente(Cache, Stats, Adresse_IP_Cache, Masque_Cache) then
-               Masque_Cache := Meilleur_Masque().mask;
-               Interface_Cache := Meilleur_Masque().inter;
+               Masque_Cache := Meilleur_Masque(table, ligne_a_lire, current_tab).mask;
+               Interface_Cache := Meilleur_Masque(table, ligne_a_lire, current_tab).inter;
                if Est_Pleine(Cache, capacite_cache) then
                   Supprimer(Cache, Politique);
                end if;
